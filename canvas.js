bdb694b7f56dbb8cf2d06780282610f440961bd0
KChenDu <kelvenchendu@gmail.com>
2021-11-17 01:32:11 -0300

    Spinning Cup version

--- canvas.js
+++ canvas.js
@@ -8,8 +8,9 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.shadowMap.enabled = true;
 document.body.appendChild( renderer.domElement );
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
-const axeshelp = new THREE.AxesHelper(5);
-scene.add(axeshelp)
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 128, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter } );
+const cubeCamera = new THREE.CubeCamera( 1, 100000, cubeRenderTarget );
+scene.add( cubeCamera );
 
 // Background texture
 const texture = new THREE.TextureLoader().load( 'img/sky1K.jpg',
@@ -23,8 +24,6 @@ const texture = new THREE.TextureLoader().load( 'img/sky1K.jpg',
 const light = new THREE.DirectionalLight( 0xFFFFFF );
 light.castShadow = true;
 light.position.set(7, 6, 0);
-const helper = new THREE.DirectionalLightHelper( light, 5 );
-scene.add( helper );
 scene.add( light );
 
 // Floor
@@ -42,117 +41,117 @@ floorMaterial.map.wrapS = THREE.RepeatWrapping;
 floorMaterial.map.wrapT = THREE.RepeatWrapping;
 floorMaterial.map.repeat.set(floorTextureRepetitions, floorTextureRepetitions);
 const floor = new THREE.Mesh( floorGeometry, floorMaterial );
-floor.rotation.x = -Math.PI / 2
+floor.rotation.x = -Math.PI / 2;
 floor.receiveShadow = true;
-// scene.add( floor );
+scene.add( floor );
+
+// Track
+const curve = new THREE.CatmullRomCurve3( [
+    new THREE.Vector3( 0, 300, 30 ),
+    new THREE.Vector3( 50, 30, 0 ),
+    new THREE.Vector3( 80, 70, 70 ),
+    new THREE.Vector3( 150, 40, -30 ),
+    new THREE.Vector3( 190, 150, 0 ),
+    new THREE.Vector3( 250, 250, 0 ),
+    new THREE.Vector3( 300, 20, 0 ),
+    new THREE.Vector3( 380, 0, 0 )
+] );
+
+const points = curve.getPoints( 10000 );
+const trackGeometry = new THREE.BufferGeometry().setFromPoints( points );
+const trackMaterial = new THREE.LineBasicMaterial( { color : 0xffffff } );
+const track = new THREE.Line( trackGeometry, trackMaterial );
+scene.add( track );
+
+// Metal cube
+const cubeGeometry = new THREE.SphereGeometry();
+const cubeMaterial = new THREE.MeshLambertMaterial( {
+    envMap: cubeCamera.renderTarget.texture
+} );
+const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+cube.castShadow = true;
+cube.position.y = 2;
+scene.add( cube );
 
 // Kart
-const GLTFLoader = new THREE.GLTFLoader();
 let kart;
-GLTFLoader.load( 'models/kart.glb', function ( gltf ) {
-    //gltf.scene.position.y = 5;
-    let i = 0;
+const GLTFLoader = new THREE.GLTFLoader();
+GLTFLoader.load( 'models/cup.glb', function ( gltf ) {
+    let c = 0;
+    // gltf.scene.position.set(points[i].x, points[i].y, points[i].z);
+    // gltf.scene.lookAt( points[i + 1].x - points[i - 1].x,
+    //     points[i + 1].y - points[i - 1].y,
+    //     points[i + 1].z - points[i - 1].z );
     gltf.scene.traverse( function( child ) {
+        // group.add( child );
         if( child.isMesh ) {
-            //console.log(i);
+            console.log(c);
             child.castShadow = true;
-            child.material = new THREE.MeshLambertMaterial( {
+            // child.receiveShadow = true;
+            // child.material = new THREE.MeshLambertMaterial( {
                 // fill color
-            } );
+            // } );
         }
-        i++;
+        c++;
     } );
     kart = gltf.scene;
-    scene.add( gltf.scene );
-    }, undefined, function ( error ) {
-    console.error( error );
-} );
-
-GLTFLoader.load( 'models/track.glb', function ( gltf ) {
-    gltf.scene.traverse( function( child ) {
-        if( child.isMesh ) {
-            child.castShadow = true;
-            child.material = new THREE.MeshBasicMaterial( {
-                color: 0xff00ff
-            } );
-        }
-    } );
+    // kart.add( sphere );
     scene.add( gltf.scene );
 }, undefined, function ( error ) {
     console.error( error );
 } );
 
+camera.position.set(150, 350, 200);
+camera.lookAt(150, 0, 0);
+// kart.children[1].position.set(3, 3, 3);
 // Keyboard Controls
 document.addEventListener('keydown', (e) => {
     //console.log(e.key)
     switch(e.key) {
         case '1':
             scene.add(camera);
-            camera.position.set(50,50,50);
-            camera.lookAt(0,0,0);
+            camera.position.set(150, 350, 200);
+            camera.lookAt(150, 0, 0);
             break;
         case '2':
-            kart.add(camera);
-            camera.position.set(0,3,0);
-            camera.setRotationFromAxisAngle(new THREE.Vector3( 0, 1, 0), Math.PI);
+            cube.add(camera);
+            camera.position.set(0,0,0);
+            camera.setRotationFromAxisAngle(new THREE.Vector3( 0, 1, 0 ), Math.PI);
             break;
         case '3':
-            kart.add(camera);
+            cube.add(camera);
             camera.position.set(0,8,-20);
-            camera.setRotationFromAxisAngle(new THREE.Vector3( 0, 1, 0), Math.PI);
+            camera.setRotationFromAxisAngle(new THREE.Vector3( 0, 1, 0 ), Math.PI);
             break;
         default:
     }
 })
 
-
-// Track - quero mobius
-
-const track = new THREE.CurvePath() // Curve paths are an array of curves
-const numOfCurves = 1;
-//let curveStart = 
-for(var i = 0; i < numOfCurves; i++) {
-    const curve = new THREE.CubicBezierCurve3(
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(-5, 15, 0),
-        new THREE.Vector3(20, 15, 0),
-        new THREE.Vector3(10, 0, 0)
-    )
-    track.add(curve);
-}
-const trackGeometry = new THREE.BufferGeometry().setFromPoints(
-    track.getPoints()
-);
-const trackMaterial = new THREE.LineBasicMaterial({color: 0xff0000});
-const trackObject = new THREE.Line(trackGeometry, trackMaterial);
-scene.add(trackObject)
-
-//##################################################### TEST ZONE dont remove
-
-const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 128, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter } );
-const cubeCamera = new THREE.CubeCamera( 1, 100000, cubeRenderTarget );
-scene.add( cubeCamera );
-
-const geometry = new THREE.SphereGeometry(2, 100, 50);
-const material = new THREE.MeshLambertMaterial({
-    envMap: cubeCamera.renderTarget.texture
-});
-const sphere = new THREE.Mesh( geometry, material );
-sphere.position.y = 2;
-scene.add( sphere );
-
-//########################################################33
-
-camera.position.y = 15;
-camera.lookAt(0, 0, 0);
+let i = 1;
 
 const animate = function () {
-    requestAnimationFrame( animate );
-    //########################## TEST ZONE dont remove
-    cubeCamera.position.copy( sphere.position );
-    cubeCamera.update( renderer, scene );
-    //##########################
-    renderer.render( scene, camera );
+        requestAnimationFrame( animate );
+        //########################## TEST ZONE dont remove
+        // kart.children[1].position.set(3,3,3);
+        // updateKart(i);
+        kart.position.copy( points[i] );
+        cube.position.copy( points[i] );
+        cube.position.y += 3;
+        cube.lookAt( points[i + 1].x - points[i - 1].x,
+                     points[i + 1].y - points[i - 1].y,
+                     points[i + 1].z - points[i - 1].z );
+        kart.rotation.y += 0.1;
+        // cube.rotation.x += 0.1;
+        // cube.rotation.z += 0.1;
+        cubeCamera.position.copy( cube.position );
+        cubeCamera.update( renderer, scene );
+        // camera.position.copy( sphere.position );
+        //##########################
+        renderer.render( scene, camera );
+        if ( i < 9999 )
+            ++i;
+        else
+            i = 1;
 };
 
 animate();
